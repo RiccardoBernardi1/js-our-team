644fe48f0ba76b920301d467a3017299be408d9c
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-07 14:04:20 +0100

    Crea un array contenente i membri del team

---
+++ js/main.js
@@ -0,0 +1,39 @@
+"use strict"
+// 1. Creare un'array contenente le info dei componenti del team
+// 2. Stampare in console nome, ruolo e la stringa della foto
+// 3. Stampare le stesse su DOM
+// 4. Trasformare la stringa della foto in un immagine effettiva
+// 5. Creare una card per ciascuno dei membri del team
+
+const team=[
+    {
+        memberName:"Wayne Barnett",
+        memberRole:"Founder & CEO",
+        memberImg:"wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        memberName:"Angela Caroll",
+        memberRole:"Chief Editor",
+        memberImg:"angela-caroll-chief-editor.jpg"
+    },
+    {
+        memberName:"Walter Gordon",
+        memberRole:"Office Manager",
+        memberImg:"walter-gordon-office-manager.jpg"
+    },
+    {
+        memberName:"Angela Lopez",
+        memberRole:"Social Media Manager",
+        memberImg:"angela-lopez-social-media-manager.jpg"
+    },
+    {
+        memberName:"Scott Estrada",
+        memberRole:"Developer",
+        memberImg:"scott-estrada-developer.jpg"
+    },
+    {
+        memberName:"Barbara Ramos",
+        memberRole:"Graphic Designer",
+        memberImg:"barbara-ramos-graphic-designer.jpg"
+    }
+]
